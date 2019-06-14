b2c3593346e311206db88af413c643eac6e3d8cf
ronbenshimol <ronbenshimol@gmail.com>
2019-06-14 17:20:32 +0300

    init project!

---
+++ app/src/main/java/com/example/simulatorcontrol/JoystickActivity.java
@@ -0,0 +1,14 @@
+package com.example.simulatorcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoystickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joystick);
+    }
+}
--- app/src/main/java/com/example/simulatorcontrol/MainActivity.java
+++ app/src/main/java/com/example/simulatorcontrol/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.simulatorcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void connect(View view){
+
+        EditText ipText = (EditText)findViewById(R.id.ipText);
+        EditText portText = (EditText)findViewById(R.id.portText);
+
+        //try to connect
+        //if success -> open joystick activity
+
+        Intent intent = new Intent(this,JoystickActivity.class);
+
+
+        intent.putExtra("",4);//temp
+        startActivity(intent);
+    }
+
+
 }
